4114aec0631056eb7322a15ddf717597ad4f9205
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-05-13 10:43:50 -0400

    Initial Commit

--- COMP123-S2016-Lesson1B/Program.cs
+++ COMP123-S2016-Lesson1B/Program.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Author: Tom Tsiliopoulos
+ * Date Created: May 13, 2016
+ * Date Modified: May 13, 2016
+ * Student ID: 30018557
+ * 
+ * Description: This program demonstrates GitHub and version control
+ * best practices. We also explore Unit Testing
+ * 
+ * Version: 0.1 - Initial Version
+ */
+
+
 namespace COMP123_S2016_Lesson1B
 {
     class Program
